f499f0e6a684654f17267f3e1c0c681e3683bc0b
EpicUsaMan <epicusaman@gmail.com>
2018-11-26 15:01:57 +0200

    Release update

---
+++ dist/src/cropperDrawSettings.d.ts
@@ -0,0 +1,10 @@
+export declare class CropperDrawSettings {
+    lineDash: boolean;
+    strokeWidth: number;
+    strokeColor: string;
+    fillColor: string;
+    dragIconStrokeWidth: number;
+    dragIconStrokeColor: string;
+    dragIconFillColor: string;
+    constructor(settings?: any);
+}
